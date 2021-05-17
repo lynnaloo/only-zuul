22019cd295cea56d80adb62d6365ac47503bb62e
Linda Nichols <lynnaloo@gmail.com>
2021-05-17 19:52:31 -0400

    a big twinkie

--- spengler/test/test.js
+++ spengler/test/test.js
@@ -3,12 +3,12 @@ const ghostbusters = ['spengler', 'venkman', 'stantz', 'zeddemore'];
 
 describe('ghostbusters', function() {
 
-  context('array of ghostbusters', function() {
-    it('is an array', function() {
-      assert.isArray(ghostbusters, 'is array of ghostbusters');
-      assert.include(ghostbusters, 'spengler', 'array contains spengler');
-      assert.lengthOf(ghostbusters, 4, 'array contains 4 ghostbusters');
-    })
-  })
+    context('array of ghostbusters', function() {
+        it('is an array', function() {
+            assert.isArray(ghostbusters, 'is array of ghostbusters');
+            assert.include(ghostbusters, 'spengler', 'array contains spengler');
+            assert.lengthOf(ghostbusters, 4, 'array contains 4 ghostbusters');
+        });
+    });
 
 });
