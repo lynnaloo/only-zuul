aaffcb27cad820b2762845d91d16dcad0f042d33
Linda Nichols <lynnaloo@gmail.com>
2021-05-17 18:42:58 -0400

    What do you mean, bad?

--- spengler/ProtonPack/index.js
+++ spengler/ProtonPack/index.js
@@ -2,9 +2,23 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     context.log('Request:\n' + JSON.stringify(req, null, 2));
 
-    const html = `<html><body>
-    <iframe src="https://giphy.com/embed/3o72FiKtrMAjIb0Rhu" width="480" height="201" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ghostbustersmovies-ghostbusters-original-3o72FiKtrMAjIb0Rhu">via GIPHY</a></p>
-      </body></html>`;
+    const bestGhostbuster = 'Spengler';
+    const html = `
+    <html>
+    <body>
+      <center>
+        <h1> The Best Ghostbuster is:</h1>
+        <br>
+        <p><iframe src="https://giphy.com/embed/3o72FiKtrMAjIb0Rhu" width="480" height="201" frameBorder="0" class="giphy-embed" allowFullScreen>
+        </iframe>
+      </center>
+      <br>
+      <p><marquee direction="right" width="40%" hspace="30%" scrollamount="14">
+      <h1>${bestGhostbuster}</h1>
+      </marquee>
+    </body>
+    </html>
+    `;
 
     context.res = {
         status: 200,
