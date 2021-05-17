94ccaa5d7af871c84e7ca22123b849605ba19a95
Linda Nichols <lynnaloo@gmail.com>
2021-05-17 18:56:30 -0400

    cats and dogs living together

--- venkman/Ecto1/index.js
+++ venkman/Ecto1/index.js
@@ -1,9 +1,25 @@
 exports.handler = async (event) => {
   console.info('Event:\n' + JSON.stringify(event, null, 2));
 
-  const html = `<html><body>
-      <iframe src="https://giphy.com/embed/l3q2HS9FG81YSdkB2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ifc-ghostbusters-l3q2HS9FG81YSdkB2">via GIPHY</a></p>
-      </body></html>`;
+  const bestGhostbuster = 'Venkman';
+  const html = `
+  <html>
+  <body>
+    <center>
+      <h2> The Best Ghostbuster is:</h2>
+      <br>
+      <p>
+        <iframe src="https://giphy.com/embed/l3q2HS9FG81YSdkB2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen>
+        </iframe>
+      </p>
+    </center>
+    <br>
+    <p><marquee direction="right" width="40%" hspace="30%" scrollamount="14">
+    <h1>${bestGhostbuster}</h1>
+    </marquee></p>
+  </body>
+  </html>
+  `;
 
     const response = {
         statusCode: 200,
